cc6521db373536e28873ded646a26785e90442da
Reiket <olinyko2607@gmail.com>
2025-03-11 18:25:13 +0200

    refactor App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import {Section} from './components/Section';
 import {Button} from '@/components/Button/Button';
 import {FavButton} from '@/components/FavButton/FavButton';
-<<<<<<< HEAD
-import {SliderNavigate} from '@/components/SliderNavigate/SliderNavigate';
-=======
 import {RouterLink} from '@/components/RouterLink/RouterLink';
-import {AiFillAccountBook} from 'react-icons/ai';
->>>>>>> 9d13291 (added Router Link component)
 
 function App() {
   return (
@@ -38,28 +33,14 @@ function App() {
         />
         <FavButton type="transparent" />
       </Section>
-<<<<<<< HEAD
-      <Section title="Slider Navigate Example">
-        <SliderNavigate />
-=======
       <Section title="Link Example">
         <RouterLink
-          withIcon
           href={'/'}
           size={'bold'}
           color={'dark'}
         >
-          <AiFillAccountBook /> request@example.com
+          Link
         </RouterLink>
-        <RouterLink
-          withIcon
-          href={'/'}
-          size={'bold'}
-          color={'dark'}
-        >
-          <AiFillAccountBook />
-        </RouterLink>
->>>>>>> 9d13291 (added Router Link component)
       </Section>
     </>
   );
