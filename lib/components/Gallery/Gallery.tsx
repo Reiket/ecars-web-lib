b8346ab31c5f8f852629ebacf6c5f2cecc7ee7a4
Reiket <olinyko2607@gmail.com>
2025-05-26 16:09:58 +0300

    fixed comments

--- lib/components/Gallery/Gallery.tsx
+++ lib/components/Gallery/Gallery.tsx
@@ -1,15 +1,17 @@
 import type {FC} from 'react';
 import type {ReactImageGalleryItem} from 'react-image-gallery';
 import ImageGallery from 'react-image-gallery';
+import type {ElementProps} from '@/services/types';
 
-interface Props {
+interface Props extends ElementProps {
   images: ReactImageGalleryItem[];
 }
 
-export const Gallery: FC<Props> = ({images}) => {
+export const Gallery: FC<Props> = ({images, block}) => {
   return (
     <ImageGallery
       items={images}
+      additionalClass={block}
       showFullscreenButton={false}
       showPlayButton={false}
     />
