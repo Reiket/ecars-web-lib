614e49f6cd2f397f4e6e5b5de50bf78219eaa4f0
Reiket <olinyko2607@gmail.com>
2025-06-12 16:12:29 +0300

    fixed code style

--- lib/components/RangeSlider/RangeSlider.tsx
+++ lib/components/RangeSlider/RangeSlider.tsx
@@ -2,11 +2,9 @@ import type {FC} from 'react';
 import type {SliderProps} from 'rc-slider';
 import Slider from 'rc-slider';
 
-export const RangeSlider: FC<SliderProps> = (props) => {
-  return (
-    <Slider
-      {...props}
-      range
-    />
-  );
-};
+export const RangeSlider: FC<SliderProps> = (props) => (
+  <Slider
+    {...props}
+    range
+  />
+);
