0f8c317ffce549f171c82abbcd3e76e194aca407
Reiket <olinyko2607@gmail.com>
2025-06-17 18:35:03 +0300

    fixed errors and added block structure

--- lib/components/Slider/Slider.tsx
+++ lib/components/Slider/Slider.tsx
@@ -3,13 +3,15 @@ import type {FC, ReactNode} from 'react';
 import {Swiper} from 'swiper/react';
 import type {SwiperModule} from 'swiper/types/shared';
 import {SLIDER_TEST_ID} from '@/components/Slider/constants';
+import {cn} from '@/services/helpers';
+import type {ElementProps} from '@/services/types';
 
 interface BreakpointSettings {
   slidesPerView: number;
   spaceBetween: number;
 }
 
-interface Props {
+interface Props extends ElementProps {
   children: ReactNode;
   spaceBetween?: number;
   slidesPerView?: number;
@@ -17,8 +19,9 @@ interface Props {
   modules?: SwiperModule[];
 }
 
-export const SliderComponent: FC<Props> = ({spaceBetween, slidesPerView, breakpoints, modules, children}) => (
+export const SliderComponent: FC<Props> = ({spaceBetween, slidesPerView, block, breakpoints, modules, children}) => (
   <Swiper
+    className={cn(block, 'slider')}
     data-testid={SLIDER_TEST_ID}
     modules={modules}
     spaceBetween={spaceBetween}
